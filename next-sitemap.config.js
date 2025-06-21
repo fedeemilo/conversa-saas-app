d3131d3440ecf209869050041a8d31efff477fef
fedeemilo <fedmilo91@gmail.com>
2025-06-21 19:01:51 -0300

    Mode enhance in seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,10 +3,14 @@ module.exports = {
 	siteUrl: 'https://eduvoice.ai',
 	generateRobotsTxt: true,
 	outDir: 'public',
-	additionalPaths: async () => [
-		{ loc: '/' },
-		{ loc: '/subscription' },
-		{ loc: '/my-journey' },
-		{ loc: '/companions/new' }
-	]
+	additionalPaths: async () => {
+		return [
+			{ loc: '/', changefreq: 'daily', priority: 1.0 },
+			{ loc: '/sign-in', changefreq: 'monthly', priority: 0.3 },
+			{ loc: '/subscription', changefreq: 'monthly', priority: 0.5 },
+			{ loc: '/my-journey', changefreq: 'weekly', priority: 0.8 },
+			{ loc: '/companions', changefreq: 'weekly', priority: 0.8 },
+			{ loc: '/companions/new', changefreq: 'monthly', priority: 0.6 }
+		]
+	}
 }
