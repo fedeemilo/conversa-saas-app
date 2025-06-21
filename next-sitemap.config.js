f75afb88bf9feaf3eb59f002e0d67ff6742a66a7
fedeemilo <fedmilo91@gmail.com>
2025-06-21 18:50:13 -0300

    Correct sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
 	siteUrl: 'https://eduvoice.ai',
-	generateRobotsTxt: true
+	generateRobotsTxt: true,
+	outDir: 'public',
+	additionalPaths: async () => [
+		{ loc: '/' },
+		{ loc: '/subscription' },
+		{ loc: '/my-journey' },
+		{ loc: '/companions/new' }
+	]
 }
